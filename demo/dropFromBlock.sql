bab00a7bb9d86197916daf9a287b60c7b4ccf47e
The Linux Kitten <linux.kitten@gmail.com>
2018-03-23 22:49:17 +0100

    Procedure mysql para eliminar información de bloque(s).

---
+++ demo/dropFromBlock.sql
@@ -0,0 +1,12 @@
+CREATE PROCEDURE dropFromBlock (IN n INTEGER UNSIGNED)
+BEGIN
+  START TRANSACTION;
+  DELETE FROM deadAccounts where blkNum >= n;
+  DELETE FROM internalTxs where blkNum >= n;
+  DELETE FROM msgCalls where blkNum >= n;
+  DELETE FROM contractCreations WHERE blkNum >= n;
+  DELETE FROM txs where blkNum >= n;
+  DELETE FROM blocks WHERE blkNum >= n;
+  COMMIT;
+END
+
